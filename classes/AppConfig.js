4b23632ce78a269af6078de25aa24b1b995bc97e
poplogics <randolph.gordon@poplogics.com>
2025-01-20 02:54:44 +0100

    Deleted files duplicated & not needed

--- classes/AppConfig.js
+++
@@ -1,5 +0,0 @@
-export const AppConfig = {
-  STORAGE_KEY: "quizData",
-  DROPDOWN_ID: "categorySelector",
-  DEBUG_MODE: true, // For controlling debug features like global quiz exposure
-};
--- classes/NotificationService.js
+++
@@ -1,9 +0,0 @@
-export class NotificationService {
-  constructor(NotificationManager) {
-    this.NotificationManager = NotificationManager;
-  }
-
-  showCategoryNotification(category) {
-    return new this.NotificationManager(category);
-  }
-}
